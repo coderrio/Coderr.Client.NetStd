451e918a5a1ec0b18f0a3dc31adf9441c1353a7f
jgauffin <jonas@gauffin.org>
2017-11-09 21:34:49 +0100

    Started working on partioning

---
+++ src/Coderr.Client.NetStd/PartitioningExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using codeRR.Client.ContextCollections;
+using codeRR.Client.Reporters;
+
+namespace codeRR.Client
+{
+    public static class PartitioningExtensions
+    {
+        public static void AddPartition(this IErrorReporterContext context, string partitionName, string partitionKey)
+        {
+            var collection = context.ContextCollections.FirstOrDefault(x => x.Name == ErrPartitions.NAME);
+            if (collection == null)
+            {
+                collection = new ErrPartitions();
+                context.ContextCollections.Add(collection);
+            }
+
+            collection.Properties.Add(partitionName, partitionKey);
+        }
+    }
+}
