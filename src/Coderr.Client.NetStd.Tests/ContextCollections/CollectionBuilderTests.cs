1166187058d56be94379298db8d1384cda51d449
jgauffin <jonas@gauffin.org>
2020-01-15 17:37:14 +0100

    Corrected tests

--- src/Coderr.Client.NetStd.Tests/ContextCollections/CollectionBuilderTests.cs
+++ src/Coderr.Client.NetStd.Tests/ContextCollections/CollectionBuilderTests.cs
@@ -4,6 +4,7 @@ using System.Security.Cryptography;
 using System.Security.Principal;
 using System.Text;
 using Coderr.Client.ContextCollections;
+using Coderr.Client.ContextCollections.Providers;
 using FluentAssertions;
 using Xunit;
 
@@ -37,7 +38,7 @@ namespace Coderr.Client.NetStd.Tests.ContextCollections
         public void CreateTokenForCredentials_should_support_domain_names()
         {
             var identity = new GenericIdentity("myjob\\someone");
-            var expected = CreateMd5Hash("someone");
+            var expected = CreateHash("someone");
 
             var actual = CollectionBuilder.CreateTokenForCredentials(identity);
 
@@ -49,7 +50,7 @@ namespace Coderr.Client.NetStd.Tests.ContextCollections
         public void CreateTokenForCredentials_should_work_without_domain_names()
         {
             var identity = new GenericIdentity("someone");
-            var expected = CreateMd5Hash("someone");
+            var expected = CreateHash("someone");
 
             var actual = CollectionBuilder.CreateTokenForCredentials(identity);
 
@@ -57,11 +58,12 @@ namespace Coderr.Client.NetStd.Tests.ContextCollections
             actual.Properties.Should().NotContainKey("DomainName");
         }
 
-        private string CreateMd5Hash(string value)
+        private string CreateHash(string value)
         {
+            const uint murmurSeed = 0xc17ca3d3;
             var buffer = Encoding.UTF8.GetBytes(value);
-            var hash = MD5.Create().ComputeHash(buffer);
-            return Convert.ToBase64String(hash).TrimEnd('=');
+            var hash = MurmurHash2.Hash(buffer, murmurSeed);
+            return hash.ToString("x");
         }
     }
 }
